9291ba4f108c88ce803104f8b11be1f0ba49a41f
FriedrichWu <wujiajund@gmail.com>
2024-10-23 11:50:10 +0200

    first change

---
+++ src/SRAMController.v
@@ -0,0 +1,199 @@
+module SRAMController (
+	input wire clk,
+	input wire rst_n,
+	// tx 
+	input wire tx_ready,
+	output reg tx_enable,
+	output reg tx_valid, 
+	output reg [7:0] tx_data_in,
+	// rx
+	input wire [7:0] rx_data_out,
+	input wire rx_valid,
+	output reg rx_enable,
+	output reg rx_ready,
+	// sram
+	output reg csb_n;
+	output reg we_n,
+	input wire [4:0] addr,
+	input wire [31:0] sram_data_out,
+	output reg [31:0] sram_data_in
+);
+//=====================================//
+//==========INTERNAL_SIGNAL============//
+//=====================================//
+localparam IDLE = 'b0000;
+localparam RD_0 = 'b0001;
+localparam RD_1 = 'b0010;
+localparam RD_2 = 'b0011;
+localparam RD_3 = 'b0100;
+localparam WD_0 = 'b0101;
+localparam WD_1 = 'b0110;
+localparam WD_2 = 'b0111;
+localparam WD_3 = 'b1000;
+localparam WRITE = 'b1001;
+reg [2:0] cur_state;
+reg [2:0] nxt_state;
+reg [7:0] addr_tmp;
+reg [31:0] data_tmp;
+wire addr_tmp_en;
+wire data_tmp_en;
+//============ADDR_TMP_REG==============//
+always @(posedge clk, negedge rst_n) begin
+	if (!rst_n) begin
+		addr_tmp <= 'b0;
+	end
+	else begin
+		if (addr_tmp_en) begin
+			addr_tmp <= rx_data_out;
+		end
+	end
+end
+//=============DATA_TMP_REG=============//
+always @(posedge clk, negedge rst_n) begin
+	if (!rst_n) begin
+		data_tmp <= 'b0;
+	end
+	else begin
+		if (data_tmp_en) begin
+			data_tmp <= {data_tmp[23:0], rx_data_out}
+		end
+	end
+end
+//======================================//
+//============STATE_MACHINE=============//
+//======================================//
+always @(posedge clk, negedge rst_n) begin
+	if (!rst_n) begin
+		cur_state <= IDLE;
+	end
+	else begin
+		cur_state <= nxt_state;
+	end
+end
+
+always @(*) begin
+	//defualt value
+	addr_tmp_en = 'b0;
+	we_n        = 'b0;
+	csb_n       = 'b1;
+	tx_enable   = 'b0;
+	tx_valid    = 'b0; 
+	rx_ready = 'b0;
+	data_tmp_en = 'b0;
+	case (cur_state)
+		IDLE: begin
+			if rx_valid begin
+				if (rx_data_out[5] == 'b1) begin // read 
+                    we_n = 'b1;
+					csb_n = 'b0;
+					addr = rx_data_out[4:0];
+					rx_ready = 'b1;
+					nxt_state = RD_0;
+				end 
+				else begin // write
+					addr_tmp_en = 'b1;// store the address
+					rx_ready = 'b1;
+					nxt_state = WD_0;
+				end
+			end
+			else begin
+				nxt_state = IDLE;
+			end
+		end 
+		RD_0: begin
+			if (tx_ready) begin
+				tx_enable = 'b1;
+				tx_data_in = sram_data_out[7:0];	
+				tx_valid = 'b1;
+				nxt_state = RD_1;
+			end
+			else begin
+				nxt_state = RD_0;
+			end
+		end
+		RD_1: begin
+			if (tx_ready) begin
+				tx_enable = 'b1;
+				tx_data_in = sram_data_out[15:8];
+				tx_valid = 'b1;
+				nxt_state = RD_2;
+			end
+			else begin
+				nxt_state = RD_1;
+			end
+		end
+		RD_2: begin
+			if (tx_ready) begin
+				tx_enable = 'b1;
+				tx_data_in = sram_data_out[23:16];
+				tx_valid = 'b1;
+				nxt_state = RD_3;
+			end
+			else begin
+				nxt_state = RD_2;
+			end
+		end
+		RD_3: begin
+			if (tx_ready) begin
+				tx_enable = 'b1;
+				tx_data_in = sram_data_out[31:24];
+				tx_valid = 'b1;
+				nxt_state = IDLE;
+			end
+			else begin
+				nxt_state = RD_3;
+			end
+		end
+		WD_0: begin
+			if (rx_valid) begin
+				data_tmp_en = 'b1;
+				rx_ready = 'b1;
+				nxt_state = WD_1;
+			end
+			else begin
+				nxt_state = WD_0;
+			end
+		end
+		WD_1: begin
+			if (rx_valid) begin
+				data_tmp_en = 'b1;
+				rx_ready = 'b1;
+				nxt_state = WD_2;
+			end
+			else begin
+				nxt_state = WD_1;
+			end
+		end
+		WD_2: begin
+			if (rx_valid) begin
+				data_tmp_en = 'b1;
+				rx_ready = 'b1;
+				nxt_state = WD_3;
+			end
+			else begin
+				nxt_state = WD_2;
+			end
+		end
+		WD_3: begin
+			if (rx_valid) begin
+				data_tmp_en = 'b1;
+				rx_ready = 'b1;
+				nxt_state = WRITE;
+			end
+			else begin
+				nxt_state = WD_3;
+			end
+		end
+		WRITE: begin
+            we_n = 'b1;
+			csb_n = 'b0;
+			addr = rx_data_out[4:0];
+			sram_data_in = data_tmp;	
+			nxt_state = IDLE;
+		end
+		default: begin
+			nxt_state = IDLE;
+		end 
+	endcase
+end
+endmodule
---
+++ src/tt_um_kmakise_sram.v
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_kmakise_sram (
+`ifdef USE_POWER_PINS
+    input             VPWR,
+    input             VGND,
+`endif
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+//the bi-direction IO
+assign uio_oe  = 8'b00010000; 
+assign uio_out[7] = 1'b0;
+assign uio_out[6] = 1'b0;
+assign uio_out[5] = 1'b0;
+assign uio_out[3] = 1'b0;
+assign uio_out[2] = 1'b0;
+assign uio_out[1] = 1'b0;
+assign uio_out[0] = 1'b0;
+//the noraml output IO
+assign uo_out[0] = 1'b0;
+assign uo_out[1] = 1'b0;
+assign uo_out[2] = 1'b0;
+assign uo_out[3] = 1'b0;
+assign uo_out[4] = 1'b0;
+assign uo_out[5] = 1'b0;
+
+wire reset;
+wire rx_enable;
+wire rx_ready;
+wire rx_valid;
+wire [7:0] rx_data_out;
+wire tx_enable;
+wire tx_valid;
+wire [7:0] tx_data_in;
+wire tx_ready;
+wire csb_n;
+wire we_n;
+wire [4:0] addr;
+wire [31:0] sram_data_in;
+wire [32:0] sram_data_out_full;
+wire [32:0] sram_data_in_full;
+wire rx_in;
+wire tx_out;
+assign sram_data_in_full = {1'b0, sram_data_in};
+assign reset = ~rst_n;
+assign ui_in[3] = rx_in;
+assign uio_out[4] = tx_out;
+//============================================//
+//=================INSTANCES==================//
+//============================================//
+UARTReceiver UARTReceiver_ins (
+    .clk (clk),      // clock
+    .reset (reset),    // reset
+    .enable (rx_enable),   // enable
+    .in(rx_in),       // RX line
+    .ready(rx_ready),    // OK to transmit
+    .out (rx_data_out),      // received data
+    .valid (rx_valid),    // RX completed
+    .error(uo_out[7]),    // frame error
+    .overrun(uo_out[6])   // overrun
+);
+
+UARTTransmitter UARTTransmitter_ins (
+    .clk (clk),      // clock
+    .reset(reset),    // reset
+    .enable(tx_enable),   // TX enable
+    .valid(tx_valid),    // start transaction
+    .in(tx_data_in),       // data to transmit
+    .out(tx_out),      // TX line
+    .ready(tx_ready)     // ready for TX
+);
+
+SRAMController SRAMController_ins (
+	.clk (clk),
+	.rst_n (rst_n),
+	// tx 
+	.tx_ready (tx_ready),
+	.tx_enable (tx_enable),
+	.tx_valid (tx_valid), 
+	.tx_data_in (tx_data_in),
+	// rx
+	.rx_data_out (rx_data_out),
+	.rx_valid (rx_valid),
+	.rx_enable (rx_enable),
+	.rx_ready (rx_ready),
+	// sram
+	.csb_n (csb_n);
+	.we_n (we_n),
+	.addr (addr),
+	.sram_data_out (sram_data_out_full[31:0]),
+	.sram_data_in (sram_data_in)
+);
+
+myconfig_sky sram_ins (
+`ifdef USE_POWER_PINS
+    .vccd1(VPWR),
+    .vssd1(VGND),
+`endif
+  .clk0(clk); // clock
+  .csb0(csb_n); // active low chip select
+  .web0(we_n); // active low write control
+  .addr0(addr);
+  .spare_wen0(1'b0); // spare mask
+  .din0(sram_data_in_full);
+  .dout0(sram_data_out_full);
+);
+endmodule
